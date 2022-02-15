465d477d55886818e9949748f5f199fbebd9fd00
sunilpokhrel1 <sunil073bex@ioepc.edu.np>
2022-02-15 16:50:05 +0545

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASSIGNMENTTASK.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Address:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtaddress" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Phone:</td>
+                    <td>
+                        <asp:TextBox ID="txtphone" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btninsert" runat="server" Text="Insert"  OnClick="btninsert_Click"> </asp:Button>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:GridView ID="grd" runat="server" AutoGenerateColumns="False" OnRowCommand="grd_RowCommand" DataKeyNames="eid"  >
+                            <Columns>
+                                <asp:BoundField DataField="eid" HeaderText="eid" InsertVisible="False" ReadOnly="True" SortExpression="eid" />
+                                <asp:BoundField DataField="ename" HeaderText="ename" SortExpression="ename" />
+                                <asp:BoundField DataField="eadress" HeaderText="eadress" SortExpression="eadress" />
+                                <asp:BoundField DataField="emob" HeaderText="emob" SortExpression="emob" />
+                            </Columns>
+                        </asp:GridView>
+                        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:TESTREGISTRATIONConnectionString %>" SelectCommand="SELECT * FROM [employee]"></asp:SqlDataSource>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASSIGNMENTTASK
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                grid_view();
+            }
+        }
+        void OpenConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+        }
+        void CloseConnection()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+        public void grid_view()
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand("employee_grid", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                grd.DataSource = ds;
+                grd.DataBind();
+            }
+            CloseConnection();
+        }
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            if (btninsert.Text == "Insert")
+            {
+                OpenConnection();
+                SqlCommand cmd = new SqlCommand("employee_insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ename", txtname.Text);
+                cmd.Parameters.AddWithValue("@eadress", txtaddress.Text);
+                cmd.Parameters.AddWithValue("@emob", txtphone.Text);
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+                grid_view();
+            }
+            else if (btninsert.Text == "Update")
+            {
+                OpenConnection();
+                SqlCommand cmd = new SqlCommand("employee_update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", ViewState["abc"]);
+                cmd.Parameters.AddWithValue("@ename", txtname.Text);
+                cmd.Parameters.AddWithValue("@eadress", txtaddress.Text);
+                cmd.Parameters.AddWithValue("@emob", txtphone.Text);
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+                grid_view();
+            }
+        }
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EDT")
+            {
+                OpenConnection();
+                SqlCommand cmd = new SqlCommand("employee_Edit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                CloseConnection();
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    txtname.Text = ds.Tables[0].Rows[0]["ename"].ToString();
+                    txtaddress.Text = ds.Tables[0].Rows[0]["eadress"].ToString();
+                    txtphone.Text = ds.Tables[0].Rows[0]["emob"].ToString();
+                }
+                grid_view();
+            }
+            else
+            {
+                OpenConnection();
+                SqlCommand cmd = new SqlCommand("employee_delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+                grid_view();
+            }
+            btninsert.Text = "Update";
+            ViewState["abc"] = e.CommandArgument;
+        }
+    }
+}
